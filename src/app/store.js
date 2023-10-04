bc3553d1d3cc5f493a1930d1eb556f1b9552db8f
bilalkhalidshaikh <muhammadbilalkhalid56@gmail.com>
2023-10-04 23:33:59 +0500

    first commit

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 // import headerSlice from '../features/common/headerSlice'
 // import headerSlice from './../features/common/headerSlice'
 // import modalSlice from '../features/common/modalSlice'
-import modalSlice from './../features/common/modalSlice'
+// import modalSlice from './../features/common/modalSlice'
 // import rightDrawerSlice from '../features/common/rightDrawerSlice'
 import rightDrawerSlice from './../features/common/rightDrawerSlice'
 // import leadsSlice from '../features/leads/leadSlice'
@@ -11,7 +11,7 @@ import leadsSlice from './../features/leads/leadSlice';
 const combinedReducer = {
   // header : headerSlice,
   rightDrawer : rightDrawerSlice,
-  modal : modalSlice,
+  // modal : modalSlice,
   lead : leadsSlice
 }
 
